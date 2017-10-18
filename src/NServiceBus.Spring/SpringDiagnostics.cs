b90366321e91bf3ba98dba6bd9172d99694169a5
Bob Langley <bob.langley@particular.net>
2017-10-18 16:32:34 -0700

    Add diagnostics feature to report if using existing application context

---
+++ src/NServiceBus.Spring/SpringDiagnostics.cs
@@ -0,0 +1,27 @@
+﻿namespace NServiceBus.Features
+{
+    /// <summary>
+    /// Adds Diagnostics information
+    /// </summary>
+    public class SpringDiagnostics : Feature
+    {
+        /// <summary>
+        /// Constructor for diagnostics feature
+        /// </summary>
+        public SpringDiagnostics()
+        {
+            EnableByDefault();
+        }
+
+        /// <summary>
+        /// Sets up diagnostics
+        /// </summary>
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            context.Settings.AddStartupDiagnosticsSection("NServiceBus.Spring", new
+            {
+                UsingExistingApplicationContext = context.Settings.HasSetting<SpringBuilder.ContextHolder>()
+            });
+        }
+    }
+}
